8e3db3e50b143f5f6880378588b3128ccdb2c3f0
EQuimper <quimperemanuel@gmail.com>
2016-04-12 11:42:32 -0600

    add loading animation

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,31 +1,49 @@
-angular.module('OWMApp', ['ngRoute'])
-		.value('ownCities', ['Calgary', 'New York', 'Chicago'])
-		.config(['$routeProvider', function ($routeProvider) {
-			$routeProvider.when('/', {
-				templateUrl: 'home.html',
-				controller: 'HomeCtrl'
-			}).when('/cities/:city', {
-				templateUrl: 'city.html',
-				controller: 'CityCtrl',
-				resolve: {
-					city: function (ownCities, $route, $location) {
-						var city = $route.current.params.city;
-						if(ownCities.indexOf(city) === -1) {
-							$location.path('/error');
-							return;
-						}
-						return city;
-					}
-				}
-			}).when('/error', {
-				template: '<p>Error - Page not Found</p>'
-			}).run(function($rootScope, $location) {
-				$rootScope.$on('$routeChangeError', function() {
-					$location.path('/error');
-				});
-			});
-		}]).controller('HomeCtrl', function ($scope) {
+(function () {
 
-}).controller('CityCtrl', function ($scope, city) {
-	$scope.city = city;
-});
+	angular.module('OWMApp', ['ngRoute', 'ngAnimate'])
+		.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider
+				.when('/', {
+					templateUrl: 'home.html',
+					controller: 'HomeCtrl'
+				})
+				.when('/cities/:city', {
+					templateUrl: 'city.html',
+					controller: 'CityCtrl',
+					resolve: {
+						city: function (owmCities, $route, $location) {
+							var city = $route.current.params.city;
+							if (owmCities.indexOf(city) == -1) {
+								$location.path('/error');
+								return;
+							}
+							return city;
+						}
+					}
+				})
+				.when('/error', {
+					template: '<p>Error - Page Not Found</p>'
+				})
+				.otherwise('/error');
+		}])
+		.run(function ($rootScope, $location, $timeout) {
+			$rootScope.$on('$routeChangeError', function () {
+				$location.path("/error");
+			});
+			$rootScope.$on('$routeChangeStart', function () {
+				$rootScope.isLoading = true;
+			});
+			$rootScope.$on('$routeChangeSuccess', function () {
+				$timeout(function () {
+					$rootScope.isLoading = false;
+				}, 1000);
+			});
+		})
+		.value('owmCities', ['New York', 'Calgary', 'Chicago'])
+		.controller('HomeCtrl', function ($scope) {
+		})
+		.controller('CityCtrl', function ($scope, $routeParams, city) {
+			$scope.city = city;
+		});
+
+}());
